03f8a9863c24f2adf6de836f37bd90c83380809a
Mario César <mariocesar.c50@gmail.com>
2020-04-06 16:22:31 -0400

    Add debugtoolbar

--- src/classroom/settings/develop.py
+++ src/classroom/settings/develop.py
@@ -2,4 +2,16 @@ from .base import *
 
 DEBUG = True
 
+INSTALLED_APPS = [
+    *INSTALLED_APPS,
+    "debug_toolbar"
+]
+
+INTERNAL_IPS = ["127.0.0.1"]
+
 AUTH_PASSWORD_VALIDATORS = []
+
+MIDDLEWARE = [
+    *MIDDLEWARE,
+    "debug_toolbar.middleware.DebugToolbarMiddleware"
+]
--- src/classroom/urls.py
+++ src/classroom/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,3 +6,9 @@ urlpatterns = [
     path("admin/docs/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns = [
+        path("__debug__", include("debug_toolbar.toolbar")),
+        *urlpatterns
+    ]
